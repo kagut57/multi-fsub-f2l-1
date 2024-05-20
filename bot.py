338d54526f745f04a3c9f161f0b2e9113ca88e70
kagut57 <156945786+kagut57@users.noreply.github.com>
2024-05-20 23:08:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,7 +38,8 @@ class Bot(Client):
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
             self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
-            sys.exit()
+            pass
+            #sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CodeXBotz")
