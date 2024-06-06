a605d271b59a7498676e95c53745c934ebdda3d0
kagut57 <156945786+kagut57@users.noreply.github.com>
2024-06-06 20:22:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,16 @@ from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
 
-from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, CHANNEL_ID, PORT
+from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, CHANNEL_ID, PORT, USER_SESSION, USER_APP_ID, USER_API_HASH
+
+class UserBot(Client):
+    def __init__(self):
+        super().__init__(
+            name="UserBot",
+            api_hash=USER_API_HASH,
+            api_id=USER_APP_ID,
+            session_string=USER_SESSION  # Assuming USER_SESSION is a session string
+        )
 
 class Bot(Client):
     def __init__(self):
@@ -24,22 +33,24 @@ class Bot(Client):
             bot_token=TG_BOT_TOKEN
         )
         self.LOGGER = LOGGER
+        self.userbot = UserBot() if USER_SESSION else None
 
     async def start(self):
         await super().start()
+        if self.userbot:
+            await self.userbot.start()
         usr_bot_me = await self.get_me()
         self.uptime = datetime.now()
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
-            test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
+            test = await self.send_message(chat_id=db_channel.id, text="Test Message")
             await test.delete()
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
             self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/CodeXBotzSupport for support")
             pass
-            #sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CodeXBotz")
@@ -60,4 +71,6 @@ class Bot(Client):
 
     async def stop(self, *args):
         await super().stop()
+        if self.userbot:
+            await self.userbot.stop()
         self.LOGGER(__name__).info("Bot stopped.")
