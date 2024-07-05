6f1c275c4e2ee0a3de0cd3fadcfa653589ac33f9
kagut57 <156945786+kagut57@users.noreply.github.com>
2024-07-05 15:23:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://lodukuwaraboy:Qwerty12@cluster0.mfey0ob.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
+DB_NAME = os.environ.get("DATABASE_NAME", "blade")
 
 """
 delay for message to delete after bot send files to user. 
